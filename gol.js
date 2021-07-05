d1725f93a34a23ac34b6723707a4d73dc7089d86
Adrian Kossmann <adrian.kossmann@posteo.de>
2021-07-05 13:37:05 +0200

    +dev

--- gol.js
+++ gol.js
@@ -1,17 +1,18 @@
-
 function GameOfLife(canvas, res) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.grid = undefined;
-    this.res = res;
+    this.res = res; /* Resolution */
     this.generation = 0;
     this.cfg = {
         columns: Math.floor(this.canvas.width/this.res),
         rows: Math.floor(this.canvas.height/this.res),
-        gridFill: "#F6F5F5",
-        gridColor: "#D3E0EA",
+        gridFill: "#FFFFFF",
+        gridColor: "#000000",
         lCellFill: "#276678",
         dCellFill: "#1687A7",
+        eCellFill: "#F6F5F5",
+        trailFill: "#00DB50",
         lineWidth: 1
     };
 
@@ -22,51 +23,57 @@ GameOfLife.prototype = {
 
     state: {
         empty: null,
-        dead: false,
-        alive: true
+        dead: "dead",
+        alive: "alive",
+        trail: "trail"
     },
 
     init: function() {
         this.grid = new Array(this.cfg.columns).fill(this.state.empty)
         .map(() => new Array(this.cfg.rows).fill(this.state.empty));
-        this.draw();
+        this.drawRect();
+        this.drawAllCells();
         document.getElementById("generation").innerHTML = this.generation;
     },
-
-    draw: function() {
-
-        this.ctx.lineWidth = this.cfg.lineWidth;
-        this.ctx.strokeStyle = this.cfg.gridColor;
+    drawRect: function() {
         this.ctx.fillStyle = this.cfg.gridFill;
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-        /* draw vertical lines */
-        for(var x = 0; x <= this.cfg.columns; x++) {
-            this.ctx.moveTo(0.5 + x * this.res, 0);
-            this.ctx.lineTo(0.5 + x * this.res, this.canvas.height);
-        }
-        /* draw horizontal lines */
-        for(var y = 0; y <= this.cfg.rows; y++) {
-            this.ctx.moveTo(0, 0.5 + y * this.res);
-            this.ctx.lineTo(this.canvas.width, 0.5 + y * this.res);
-        }
-        this.ctx.stroke();
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    // drawGrid: function() {
+    //     this.ctx.lineWidth = this.cfg.lineWidth;
+    //     this.ctx.strokeStyle = this.cfg.gridColor;
 
-        /* draw cells */
-
+    //     /* draw vertical lines */
+    //     for(var x = 0; x <= this.cfg.columns; x++) {
+    //         this.ctx.moveTo(0.5 + x * this.res, 0);
+    //         this.ctx.lineTo(0.5 + x * this.res, this.canvas.height);
+    //     }
+    //     /* draw horizontal lines */
+    //     for(var y = 0; y <= this.cfg.rows; y++) {
+    //         this.ctx.moveTo(0, 0.5 + y * this.res);
+    //         this.ctx.lineTo(this.canvas.width, 0.5 + y * this.res);
+    //     }
+    //     this.ctx.stroke();
+    // },
+    drawCell: function(x, y) {
+        /* Get color */
+        if(this.grid[x][y] === this.state.empty) {
+            this.ctx.fillStyle = this.cfg.eCellFill;
+        } else if(this.grid[x][y] === this.state.alive) {
+            this.ctx.fillStyle = this.cfg.lCellFill;
+        } else if(this.grid[x][y] === this.state.dead) {
+            this.ctx.fillStyle = this.cfg.dCellFill;
+        } else {
+            this.ctx.fillStyle = this.cfg.trailFill;
+        }
+        this.ctx.fillRect( x * this.res ,  y * this.res, this.res, this.res);
+    },
+    drawAllCells: function() {
         for(let x = 0; x < this.cfg.columns; x++) {
             for(let y = 0; y < this.cfg.rows; y++) {
-                if(this.grid[x][y] === this.state.alive) {
-                    this.ctx.fillStyle = this.cfg.lCellFill;
-                    this.ctx.fillRect(0.5 + x * this.res, 0.5 + y * this.res,this.res,this.res);
-                } 
-                if(this.grid[x][y] === this.state.dead) {
-                    this.ctx.fillStyle = this.cfg.dCellFill;
-                    this.ctx.fillRect(0.5 + x * this.res, 0.5 + y * this.res,this.res,this.res);
-                }
+                this.drawCell(x,y);
             }
         }
-
-
     },
     clear: function() {
         for(let x = 0; x < this.cfg.columns; x++) {
@@ -75,11 +82,12 @@ GameOfLife.prototype = {
             }
         }
         this.generation = 0;
+        this.drawAllCells();
         document.getElementById("generation").innerHTML = this.generation;
-        this.draw();
     },
     insertCell: function(x, y, state) {
         this.grid[x][y] = state;
+        this.drawCell(x,y);
     },
     update: function() {
 
@@ -104,19 +112,18 @@ GameOfLife.prototype = {
                    if(nNeighbors < 2 || nNeighbors > 3) {
                        gridCopy[x][y] = this.state.dead;
                    }
-               } else {
-                   if(nNeighbors === 3) {
-                       gridCopy[x][y] = this.state.alive;
-                   }
-               }
-
+               } else if (nNeighbors === 3) {
+                gridCopy[x][y] = this.state.alive;
+               } else if((this.grid[x][y] === this.state.dead && nNeighbors < 3) || this.grid[x][y] === this.state.trail) {
+                   gridCopy[x][y] = this.state.trail;
+               } 
            }
        }
 
        this.grid = gridCopy;
        this.generation++;
        document.getElementById("generation").innerHTML = this.generation;
-       this.draw();
+       this.drawAllCells();
     },
     countNeighbours: function(x,y) {
 
@@ -301,7 +308,7 @@ GameOfLife.prototype = {
                 this.grid[x][y] = this.state[keys[Math.floor(Math.random() * (max - min +1)) + min]];
             }
         }
-        this.draw();
+        this.drawAllCells();
     }
 }
 
@@ -320,25 +327,40 @@ function onClick(e) {
     } else {
         game.insertCell(x,y, game.state.empty);
     }
+}
 
-    game.draw();
+let timer = null;
+let fps = 0;
+let lastCalledTime = null;
+
+function loop() {
+
+    document.getElementById("fps").innerHTML = fps;
+
+    if(!lastCalledTime) {
+        lastCalledTime = performance.now();
+        game.update();
+        timer = requestAnimationFrame(() => loop());
+    } 
+    var delta = (performance.now() - lastCalledTime)/1000;
+    lastCalledTime = performance.now();
+    fps = Math.floor(1/delta,2);
+    game.update();
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(() => loop());
 }
 
 var game = new GameOfLife(document.getElementById("canvas"), 5);
-const updateRate = 10;
-var timer;
 
 game.canvas.addEventListener('click', onClick); /* Add cells by clicking on grid */
-document.getElementById("bStart").addEventListener('click', function() {
-    timer = window.setInterval(() => game.update(), updateRate)
-}); /* Start game, set update rate */
+document.getElementById("bStart").addEventListener('click', () => loop()); /* Start game, set update rate */
 document.getElementById("bStep").addEventListener('click', () => game.update()) /* Update game for one step */
-document.getElementById("bStop").addEventListener('click', () => window.clearInterval(timer)); /* Stop game */
-document.getElementById("bClear").addEventListener('click', function() {
+document.getElementById("bStop").addEventListener('click', () => cancelAnimationFrame(timer)); /* Stop game */
+document.getElementById("bClear").addEventListener('click', () => {
     game.clear();
-    window.clearInterval(timer)}); /* Clear grid */
+    cancelAnimationFrame(timer)}); /* Clear grid */
 document.getElementById("bRandom").addEventListener('click', () => {game.clear(), window.clearInterval(timer), game.random()}); /* Fill grid randomly */
-
+document.getElementById("fps").innerHTML = fps;
 
 
 
